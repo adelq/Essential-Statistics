15bb88aee160bcc08244a49576005542b105ed4c
Adel Qalieh <abq911@gmail.com>
2011-09-03 00:25:28 -0400

    Changed var names for consistency

--- js/z.js
+++ js/z.js
@@ -1,20 +1,20 @@
 function normalcdf(form)
 {
-z = form.pbox2.value
+z = form.z.value
 t = (z > 0) ? z : (- z)
 p = 1 - Math.pow((1 + (t * (0.0498673470 + t * (0.0211410061 + t *
 (0.0032776263 + t * (0.0000380036 + t * (0.0000488906 + t *
 0.0000053830))))))), -16) / 2
-form.zbox2.value = (z > 0) ? p : (1 - p)
+form.p.value = (z > 0) ? p : (1 - p)
 }
 
 function invnorm(form)
 {
-p = form.prob.value
+p = form.p.value
 t = (p > .5) ? (1 - p) : p
 s = Math.sqrt(-2.0 * Math.log(t))
 a = 2.515517 + (0.802853 * s) + (0.010328 * s * s)
 b = 1 + (1.432788 * s) + (0.189269 * s * s) + (0.001308 * s * s * s)
 u = s - (a / b)
-form.zval.value = (p < .5) ? (- u) : u
+form.z.value = (p < .5) ? (- u) : u
 }
